0ae2dc0889a4e6d4246e3d483501647cb2c4497d
Joel Mathew V <joelmathewv0819@gmail.com>
2023-02-22 00:15:24 +0530

    recursion questions

--- recursion/Recursion.java
+++ recursion/Recursion.java
@@ -7,7 +7,31 @@ public class Recursion {
         printInc(n-1);
         System.out.println(n);
     }
+
+    public static int factorial(int n){
+        if (n==2){
+            return 2;
+        }
+         return factorial(n-1)*n;
+    }
+
+    public static int sumNatural(int n){
+        if (n == 1){
+            return 1;
+        }
+        return sumNatural(n-1)+n;
+    }
+
+    public static int fibonacci(int n){
+        if (n == 0){
+            return 0;
+        }
+        else if(n == 1){
+            return 1;
+        }
+        return fibonacci(n-2)+ fibonacci(n-1);
+    }
     public static void main(String[] args) {
-        printInc(5);
+        System.out.println( fibonacci(5));
     }
 }
